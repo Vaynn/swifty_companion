bd820f9231d21eb62a4ff74bde492262f00f6fd5
Vaynn <zavijanagmail.com>
2021-04-15 10:51:50 +0200

    Initial Commit

---
+++ swifty_companion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swifty_companion
+//
+//  Created by Yvann Martorana on 15/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
